a199a4e34220d3fb9a859293fe814f4657b2d6f0
Sascha Grollmisch <goh@idmt.fraunhofer.de>
2016-02-09 15:45:12 +0100

    styled audio player via bootstrap

---
+++ webpackages/de.fraunhofer.idmt.audio.general/audio-player/js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+(function() {
+
+    document.addEventListener('cifReady', function() {
+        $("head").append('<meta name="viewport" content="width=device-width, initial-scale=1">');
+        
+        $('div[cubx-core-crc]').addClass('container');
+        
+        $('audio-player').wrap( "<div id='audioPlayerFrame'></div>" );
+        
+        //file inputs
+        $('audio-player').children('[member-id="inputText"], [member-id="selectFile"]').wrapAll( "<div class='row vertical-align'></div>" );
+        var inputTextFile = $('[member-id="inputText"]');
+        inputTextFile.addClass('col-md-6');
+        inputTextFile.find('article').children().wrapAll( "<div class='row vertical-align'></div>" );
+        inputTextFile.find('label').wrap( "<div class='col-md-1'></div>" );
+        inputTextFile.find('label').addClass('label');
+        inputTextFile.find('input').wrap( "<div class='col-md-11'></div>" );
+        inputTextFile.find('input').addClass('form-control');
+        $('[member-id="selectFile"]').addClass('col-md-6');
+        $('[member-id="selectFile"]').find('input').addClass('form-control');
+        
+        //button bar
+        $('button-bar').wrap("<div class='row vertical-align'></div>" );
+        $('button-bar').addClass('col-md-4');
+        $('button-bar').children('button').wrapAll( "<div class='row vertical-align'></div>" );
+        $('button-bar').find('button').addClass('btn col-md-4');
+        
+        //we could also search for all sliders but this way is more stable and could be used in bde
+        $('[member-id="volumeSlider"]').addClass('col-md-3');
+        $('[member-id="bassSlider"]').addClass('col-md-3');
+        $('[member-id="middleSlider"]').addClass('col-md-3');
+        $('[member-id="trebleSlider"]').addClass('col-md-3');
+        
+        $('[member-id="volumeSlider"]').find('article').children().wrapAll( "<div class='row vertical-align'></div>" );
+        $('[member-id="bassSlider"]').find('article').children().wrapAll( "<div class='row vertical-align'></div>" );
+        $('[member-id="middleSlider"]').find('article').children().wrapAll( "<div class='row vertical-align'></div>" );
+        $('[member-id="trebleSlider"]').find('article').children().wrapAll( "<div class='row vertical-align'></div>" );
+        
+        $('slider-number *').find('label').addClass('col-md-1');
+        $('slider-number *').find('input').addClass('col-md-11');
+    });
+})();
